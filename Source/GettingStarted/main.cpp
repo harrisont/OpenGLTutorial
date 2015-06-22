0ffc0013dc2fa8dcf5f0934a52fa129c13191d7a
Harrison Ting <runhappy@gmail.com>
2015-06-22 01:33:07 -0700

    More window setup

--- Source/GettingStarted/main.cpp
+++ Source/GettingStarted/main.cpp
@@ -4,7 +4,9 @@
 // GLFW
 #include <GLFW/glfw3.h>
 
-bool InitWindow()
+#include <iostream>
+
+bool InitGlfw()
 {
     if (glfwInit() == GL_FALSE)
     {
@@ -13,16 +15,56 @@ bool InitWindow()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    return true;
+}
+
+bool InitWindow()
+{
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    const int width = 800;
+    const int height = 600;
+    const char* const title = "LearnOpenGL";
+    GLFWwindow* const window = glfwCreateWindow(width, height, title, nullptr /*monitor*/, nullptr /*share*/);
+    if (!window)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        return false;
+    }
+
+    glfwMakeContextCurrent(window);
+    return true;
+}
+
+bool InitGlew()
+{
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK)
+    {
+        std::cout << "Failed to initialize GLEW" << std::endl;
+        return false;
+    }
     return true;
 }
 
 int main()
 {
+    if (!InitGlfw())
+    {
+        return 1;
+    }
+
     if (!InitWindow())
     {
         return 1;
     }
+
+    if (!InitGlew())
+    {
+        return 1;
+    }
+
+    glfwTerminate();
 
     return 0;
 }
