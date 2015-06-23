bf48f321edce739a0e32222daca7e2d95eb42f2e
Harrison Ting <runhappy@gmail.com>
2015-06-22 22:57:13 -0700

    Refactor rendering into its own function

--- Source/GettingStarted/main.cpp
+++ Source/GettingStarted/main.cpp
@@ -52,15 +52,17 @@ void KeyCallback(GLFWwindow* const window, const int key, const int /*scancode*/
     }
 }
 
+void Render()
+{
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+}
+
 void MainLoop(GLFWwindow* const window)
 {
     while (!glfwWindowShouldClose(window))
     {
         glfwPollEvents();
-
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
         glfwSwapBuffers(window);
     }
 }
