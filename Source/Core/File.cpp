45a3135327e717202d7ee89706e9a790548affd5
Harrison Ting <runhappy@gmail.com>
2015-06-23 23:19:15 -0700

    Implement FileRead

--- Source/Core/File.cpp
+++ Source/Core/File.cpp
@@ -1,6 +1,22 @@
 #include "File.h"
+#include <fstream>
+#include <cstdint>
 
-std::string FileRead(const char* const /*filePath*/)
+std::string FileRead(const char* const filePath)
 {
-    return "";
+    std::ifstream in(filePath, std::ios::in | std::ios::binary);
+    if (!in)
+    {
+        throw("Failed to open file.");
+    }
+
+    // Get the file size
+    in.seekg(0, std::ios::end);
+    const auto size = static_cast<uint32_t>(in.tellg());
+    in.seekg(0, std::ios::beg);
+
+    std::string contents;
+    contents.resize(size);
+    in.read(&contents[0], size);
+    return contents;
 }
--- Source/Core/File.h
+++ Source/Core/File.h
@@ -1,4 +1,5 @@
 #pragma once
 #include <string>
 
+// Max file size that can be read is 2^32 bits.
 std::string FileRead(const char* const filePath);
